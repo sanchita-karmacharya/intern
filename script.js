5d5072c48a31b684cdbb34b6babb39c176e918f5
Sanchita <62978038+Newasanchita@users.noreply.github.com>
2021-10-23 10:46:15 +0545

    first

---
+++ script.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+	var slider = tns({
+			container: '.arrow',
+			items: 6,
+			slideBy: 'page',
+			nav: false,
+			mouseDrag: true
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
